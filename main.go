379a8771ef1d379b92fbc88a25e2e1037b58b1b7
bbright50 <165122465+bbright50@users.noreply.github.com>
2024-09-04 01:36:16 -0600

    updated server code to be able to run

--- main.go
+++ main.go
@@ -10,16 +10,32 @@ import (
 func main() {
 	m := http.NewServeMux()
 
-	const addr = ":8000"
+	m.HandleFunc("/", handlePage)
 
+	const addr = ":8080"
 	srv := http.Server{
-		Handler: m,
-		Addr: addr,
+		Handler:      m,
+		Addr:         addr,
 		WriteTimeout: 30 * time.Second,
-		ReadTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
 	}
 
-	fmt.Println("server started on port ", addr)
+	// this blocks forever, until the server
+	// has an unrecoverable error
+	fmt.Println("server started on ", addr)
 	err := srv.ListenAndServe()
 	log.Fatal(err)
+}
+
+func handlePage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(200)
+	const page = `<html>
+<head></head>
+<body>
+	<p> Hello from Docker! I'm a Go server. </p>
+</body>
+</html>
+`
+	w.Write([]byte(page))
 }
